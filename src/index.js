3d5d7336135655f042af88ecc82bd42209542bca
Farasat Ali <63093876+farasatAli1@users.noreply.github.com>
2020-10-21 13:50:29 +0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app/layouts/App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// For Hot Module which adds the change without refreshing or flickering of the page and we use it only in development
+const rootEl = document.getElementById("root");
+
+let render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootEl
+  );
+};
+
+if (module.hot) {  // module is from webpack
+  module.hot.accept("./app/layouts/App", () => {
+    setTimeout(render);
+  });
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
